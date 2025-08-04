7c55c5e52c9e050b3e65b2d4c2d728765a26b366
djam <djaconseil@gmail.com>
2025-08-04 16:54:56 +0200

    apelle fonction creation table dans predict_loop.py

--- ml/ohlcv_5m/predict_loop.py
+++ ml/ohlcv_5m/predict_loop.py
@@ -1,10 +1,14 @@
 import time
-from predict import main, should_predict
+from predict import main, should_predict, create_predictions_table_if_needed
 
 WAIT_SECONDS = 30  # vérifie toutes les 30 secondes
 
 if __name__ == "__main__":
     print("Démarrage du service de prédiction synchronisé...")
+    
+    # créer la table si elle n'existe pas
+    create_predictions_table_if_needed()
+    
     while True:
         if should_predict():
             print("Nouvelle bougie détectée. Prédiction en cours...")
